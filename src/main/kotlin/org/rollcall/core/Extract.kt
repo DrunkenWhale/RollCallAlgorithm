f7bcc91a4bf832efd19fe2bf0e4898d97f262505
Pigeon377 <892197240@qq.com>
2022-10-07 00:09:25 +0800

    docs(README.md): update docs

--- src/main/kotlin/org/rollcall/core/Extract.kt
+++ src/main/kotlin/org/rollcall/core/Extract.kt
@@ -2,19 +2,26 @@ package org.rollcall.core
 
 import org.rollcall.input.Input
 import org.rollcall.output.Output
+import kotlin.random.Random
 
 
 fun extractSample(input: Input, output: Output, rollCallNumber: Int) {
     val (data, credit) = input.read()
 
-    val currentList = (0..credit.size)
+    val orderList = (0..credit.size)
         .toList()
         .zip(credit)
         .sortedBy { it.second }
+
+    val currentList = orderList
         .subList(0, rollCallNumber)
         .map { it.first }
 
     val rollCallSchema = data.map { _ ->
+//        val randomAbsenceList = (1..3).map { _ ->
+//            orderList[Random.nextInt(rollCallNumber, rollCallNumber + 7)]
+//        }.map { it.first }
+//        credit.indices.map { if (it in currentList || it in randomAbsenceList) 1 else 0 }
         credit.indices.map { if (it in currentList) 1 else 0 }
     }
 

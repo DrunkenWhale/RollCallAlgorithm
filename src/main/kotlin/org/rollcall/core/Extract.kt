fd39ef99be6d5f8ad29c54df1f86755e626ed4bc
Gallon <2833275565@qq.com>
2022-10-06 15:03:07 +0800

    style: rename core algorithm

--- src/main/kotlin/org/rollcall/core/Extract.kt
+++ src/main/kotlin/org/rollcall/core/Extract.kt
@@ -3,10 +3,8 @@ package org.rollcall.core
 import org.rollcall.input.Input
 import org.rollcall.output.Output
 
-// 不好意思，🧔不知道取什么名字好 你要是看到了就取一下
-// 下面那个函数也是
 
-fun temp(input: Input, output: Output) {
+fun extractSample(input: Input, output: Output) {
     val (data, credit) = input.read()
 
 

b5bee31bf600adbc0e83f3a086d8e7b66340d844
Gallon <2833275565@qq.com>
2022-10-06 20:16:39 +0800

    feat: algorithm implementation

--- src/main/kotlin/org/rollcall/core/Extract.kt
+++ src/main/kotlin/org/rollcall/core/Extract.kt
@@ -7,15 +7,42 @@ import org.rollcall.output.Output
 fun extractSample(input: Input, output: Output) {
     val (data, credit) = input.read()
 
-    val list = (1..credit.size)
+    val rollList= mutableListOf<MutableList<Int>>()
+
+    var currentList = (1..credit.size)
         .toList()
         .zip(credit)
         .sortedBy {
             it.second
         }
         .subList(0, 10)
+        .map {
+            it.first
+        }
+
+    rollList.add(currentList as MutableList<Int>)
+
+    for (lesson in data){
+        val captureList=(1..credit.size).toList().union(currentList)
+        val restNumber=10-captureList.size
+
+        val restRollList =  (1..credit.size)
+            .toList()
+            .subtract(currentList.toSet())
+            .zip(credit)
+            .sortedBy {
+                it.second
+            }
+            .subList(0, restNumber)
+            .map {
+                it.first
+            }
+        currentList = (captureList+restRollList).toList()
+        rollList.add(currentList as MutableList<Int>)
+    }
+
 
     //TODO (implement your algorithm)
 
-    output.write(data)
+    output.write(rollList)
 }

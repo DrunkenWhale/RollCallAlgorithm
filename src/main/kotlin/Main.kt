d24da2296acd656f0b2ce45816076a068ad78b06
Pigeon377 <892197240@qq.com>
2022-10-06 15:25:42 +0800

    Feat(input): rename CsvInput => DsvInput, allow user choose separator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
-import org.rollcall.core.temp
-import org.rollcall.input.CsvInput
+import org.rollcall.core.extractSample
+import org.rollcall.input.DsvInput
 import org.rollcall.output.ConsoleOutput
 import java.nio.file.Files
 import java.nio.file.Path
@@ -7,6 +7,6 @@ import java.nio.file.Path
 fun main(args: Array<String>) {
     val pathString = "data"
     Files.list(Path.of(pathString)).forEach { path ->
-        temp(CsvInput(path), ConsoleOutput())
+        extractSample(DsvInput(path), ConsoleOutput())
     }
 }
--- src/main/kotlin/org/rollcall/input/DsvInput.kt
+++ src/main/kotlin/org/rollcall/input/DsvInput.kt
@@ -4,20 +4,21 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 
-class CsvInput(
-    val path: Path
+class DsvInput(
+    val path: Path,
+    val sep: String = ","
 ) : Input {
 
     override fun read(): Pair<List<Lesson>, List<Double>> {
         val lines = Files.readAllLines(path)
 
         val gpaList = lines[0]
-            .split(",")
+            .split(sep)
             .map { it.toDouble() }
 
         val lessonList = lines.subList(1, lines.size)
             .map { line ->
-                line.split(",")
+                line.split(sep)
                     .map { str -> str.toInt() }
             }
 

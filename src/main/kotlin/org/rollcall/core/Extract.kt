fcc6cc9ca68fbe018cd04c1d482c81988d834b8b
Pigeon377 <892197240@qq.com>
2022-10-06 22:19:01 +0800

    reafctor(alg): refactor extractSample function

--- src/main/kotlin/org/rollcall/core/Extract.kt
+++ src/main/kotlin/org/rollcall/core/Extract.kt
@@ -3,55 +3,20 @@ package org.rollcall.core
 import org.rollcall.input.Input
 import org.rollcall.output.Output
 
-fun indexesToMultiShot(list: List<Int>, len: Int): MutableList<Int> {
-    val res = mutableListOf<Int>()
-    for (i in 0 until len) {
-        if (i in list) {
-            res.add(1)
-        } else {
-            res.add(0)
-        }
-    }
-    return res
-}
 
 fun extractSample(input: Input, output: Output, rollCallNumber: Int) {
     val (data, credit) = input.read()
 
-    val rollList = mutableListOf<MutableList<Int>>()
-
-    var currentList = (0..credit.size)
+    val currentList = (0..credit.size)
         .toList()
         .zip(credit)
         .sortedBy { it.second }
         .subList(0, rollCallNumber)
-        .map {
-            it.first
-        }
+        .map { it.first }
 
-
-    for (lesson in data) {
-        rollList.add(indexesToMultiShot(currentList, credit.size))
-        val captureList = (1..credit.size).intersect(currentList.toSet()).toList()
-        val restNumber = rollCallNumber - captureList.size
-
-        val restRollList = (1..credit.size)
-            .toList()
-            .subtract(currentList.toSet())
-            .zip(credit)
-            .sortedBy {
-                it.second
-            }
-            .subList(0, restNumber)
-            .map {
-                it.first
-            }
-        currentList = (captureList + restRollList).toList()
-
+    val rollCallSchema = data.map { _ ->
+        credit.indices.map { if (it in currentList) 1 else 0 }
     }
 
-
-    //TODO (implement your algorithm)
-
-    output.write(rollList)
+    output.write(rollCallSchema)
 }

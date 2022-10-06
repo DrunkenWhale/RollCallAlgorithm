e85cadbbe613f5725cdbd3b75e4b5379d7c2a3fe
Pigeon377 <892197240@qq.com>
2022-10-06 19:23:40 +0800

    Feat(alg): part of algorithm

--- src/main/kotlin/org/rollcall/core/Extract.kt
+++ src/main/kotlin/org/rollcall/core/Extract.kt
@@ -7,6 +7,14 @@ import org.rollcall.output.Output
 fun extractSample(input: Input, output: Output) {
     val (data, credit) = input.read()
 
+    val list = (1..credit.size)
+        .toList()
+        .zip(credit)
+        .sortedBy {
+            it.second
+        }
+        .subList(0, 10)
+
     //TODO (implement your algorithm)
 
     output.write(data)

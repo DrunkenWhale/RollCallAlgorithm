572da7e56f41e91ca8c7a1df484751683176f6c2
Pigeon377 <892197240@qq.com>
2022-10-11 11:32:38 +0800

    refactor(output): refactor JsonOutput, let it extends TextOutput

--- src/main/kotlin/org/rollcall/output/JsonOutput.kt
+++ src/main/kotlin/org/rollcall/output/JsonOutput.kt
@@ -2,41 +2,20 @@ package org.rollcall.output
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.StandardOpenOption
 
-
-private const val resultStorageDir = "res"
-
-
 class JsonOutput(
-    val outputFileName: String = "result.json"
-) : Output {
-
-    private val data = mutableListOf<List<Lesson>>()
-
-    override fun write(data: List<Lesson>) {
-        this.data.add(data)
-    }
-
-    override fun read(): List<List<Lesson>> {
-        return this.data
-    }
+    val outputJsonFileName: String = "result.json"
+) : TextOutput(outputJsonFileName) {
 
     override fun output(e: Double) {
-        if (!Files.exists(Path.of(resultStorageDir))) {
-            Files.createDirectories(Path.of(resultStorageDir))
-        }
 
-        val filePath = Path.of(resultStorageDir + System.getProperty("file.separator") + outputFileName)
+        val filePath = getFilePath(outputJsonFileName)
 
-        if (Files.exists(filePath)) {
-            Files.delete(filePath)
-        }
-
-        Files.write(
-            filePath, Json.encodeToString(Res(e, data)).toByteArray(Charsets.UTF_8), StandardOpenOption.CREATE
+        super.outputToFile(
+            filePath,
+            Json.encodeToString(Res(e, data)).toByteArray(Charsets.UTF_8),
+            StandardOpenOption.TRUNCATE_EXISTING
         )
     }
 

39acd2fa22997e5164287229a55e7d638cfab585
Pigeon377 <892197240@qq.com>
2022-10-11 11:34:22 +0800

    feat(Main.kt): add reGenerateDataSet method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,48 @@
-import org.rollcall.alg.calculateE
-import org.rollcall.alg.knn
-import org.rollcall.alg.onlyRollCallFrequentlyAbsentStudents
-import org.rollcall.alg.rollCallStudentsPartlyBasedOnGpa
+import org.rollcall.alg.*
 import org.rollcall.core.extractSample
 import org.rollcall.input.DsvInput
 import org.rollcall.output.ConsoleOutput
 import org.rollcall.output.JsonOutput
+import org.rollcall.output.Output
+import org.rollcall.output.TextOutput
 import java.nio.file.Files
 import java.nio.file.Path
 
-fun main(args: Array<String>) {
+fun main() {
 
-    val rollCallNumber = 30
+    val rollCallFunction: Map<String, RollCallSchema> = mapOf(
+        Pair("onlyRollCallFrequentlyAbsentStudents", onlyRollCallFrequentlyAbsentStudents),
+        Pair("rollCallStudentsPartlyBasedOnGpa", rollCallStudentsPartlyBasedOnGpa),
+        Pair("knn", knn)
+    )
+
+    rollCallFunction.forEach { (fName, f) ->
+        reGenerateDataSet()
+        walk(f, ConsoleOutput())
+        walk(f, JsonOutput("$fName.json"))
+        walk(f, TextOutput(fName))
+    }
+
+}
+
+fun reGenerateDataSet() {
+    // 这里是一个异步调用
+    // 如果不阻塞到生成完毕
+    // 会导致只读到前60%的数据
+    // (只有60%也能跑 这是不是说明我写的很合理捏)
+    // 正确率计算没有可信度
+    Runtime.getRuntime().exec("./executable/gen").waitFor()
+//    Thread.sleep(1000)
+}
+
+fun walk(f: RollCallSchema, output: Output) {
+    val rollCallNumber = 7
     val pathString = "data"
-    val output = ConsoleOutput()
     val dsvInputList = Files.list(Path.of(pathString)).map { path ->
         DsvInput(path)
     }.toList()!!
 
-    dsvInputList.forEach { extractSample(knn, it, output, rollCallNumber) }
+    dsvInputList.forEach { extractSample(f, it, output, rollCallNumber) }
 
     val rollCallScheme = output.read()
 

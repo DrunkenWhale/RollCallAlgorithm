5f3cfb9bb60911ffede651017ba54e8045181e5f
Pigeon377 <892197240@qq.com>
2022-10-11 12:05:08 +0800

    feat(draw): implement data draw

---
+++ draw/draw_image.py
@@ -0,0 +1,22 @@
+from ast import main
+from cProfile import label
+from matplotlib import pyplot as plt
+import os
+
+def read(filePath:str) -> list[float]:
+    with open(filePath,"r") as f:
+        return list(map(lambda x:float(x),f.readlines()))
+    
+
+if __name__ == "__main__":
+    size = 10
+    linewidth = 3
+    prefix = "../res"
+    fileList = filter(lambda x:".json" not in x,list(os.listdir(prefix)))
+    y =  [read(prefix+os.sep+fileName) for fileName in fileList]
+    plt.plot([i for i in range(1,size+1)],y[0],linewidth=linewidth,label="onlyRollCallFrequentlyAbsentStudents")
+    plt.plot([i for i in range(1,size+1)],y[1],linewidth=linewidth,label="rollCallStudentsPartlyBasedOnGpa")
+    plt.plot([i for i in range(1,size+1)],y[2],linewidth=linewidth,label="knn")
+    plt.legend()
+    # plt.show()
+    plt.savefig("res.jpg")
